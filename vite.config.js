7749b39c6a2bfc5aa39b10803a3772993d7e214a
rocktim <rocktimsharma82@gmail.com>
2024-01-15 13:37:18 +0530

    chnages in vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   jsx: 'react',
   build: {
-    outDir: 'dist', // Set your desired build directory name
+    outDir: 'dist',
   },
 })

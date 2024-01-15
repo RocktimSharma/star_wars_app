16421ef1fe7b7c7a82465473a4b8772f7b1b7389
rocktim <rocktimsharma82@gmail.com>
2024-01-15 13:18:45 +0530

    chnages in vite config

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import { store, persistor } from "./redux/store";
+import { store, persistor } from "./redux/store.jsx";
 
 ReactDOM.render(
   <Provider store={store}>

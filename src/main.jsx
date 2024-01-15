5606ec273c2ad335c8d421646e6e258b6ee06136
rocktim <rocktimsharma82@gmail.com>
2024-01-15 12:51:11 +0530

    changes in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./redux/store";
-ReactDOM.createRoot(document.getElementById("root")).render(
+
+ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <App />
     </PersistGate>
-  </Provider>
+  </Provider>,
+  document.getElementById("root")
 );
